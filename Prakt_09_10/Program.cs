2d59ab6437e17be685d7c1c67a455daa01ffa15d
Alex Yelienok <aelenok@gmail.com>
2023-10-09 19:48:48 +0200

    Додано практику від 09/10

---
+++ Prakt_09_10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+ShowMessage showMessageObj = new ShowMessage("ShowMessage.");
+
+MessageDelegate showMessage = showMessageObj.DisplayMessage;
+
+showMessage("Це перше повідомлення через делегат.");
+showMessage("А це друге повідомлення через делегат.");
+
+Console.ReadKey();
---
+++ Prakt_09_10/ShowMessage.cs
@@ -0,0 +1,14 @@
+﻿delegate void MessageDelegate(string message);
+internal class ShowMessage
+{
+    private string v;
+
+    public ShowMessage(string v)
+    {
+        this.v = v;
+    }
+    internal void DisplayMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
