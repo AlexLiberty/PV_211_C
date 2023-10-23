092a8bece59187b3fabdc3b4bb6fc091650c52e3
Alex Yelienok <aelenok@gmail.com>
2023-10-23 14:10:30 +0200

    Додано дз_15

---
+++ DZ_15/MyArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_15
+{
+    internal class MyArray
+    {
+        private int[] numbers;
+
+        public MyArray(int[] inputArray)
+        {
+            numbers = inputArray;
+        }
+        public int[] GetAllIntegers()
+        {
+            return numbers;
+        }
+        public int[] GetEvenNumbers()
+        {
+            return numbers.Where(x => x % 2 == 0).ToArray();
+        }
+        public int[] GetOddNumbers()
+        {
+            return numbers.Where(x => x % 2 != 0).ToArray();
+        }
+        public int[] GetNumbersGreaterThan(int value)
+        {
+            return numbers.Where(x => x > value).ToArray();
+        }
+        public int[] GetNumbersInRange(int start, int end)
+        {
+            return numbers.Where(x => x >= start && x <= end).ToArray();
+        }
+        public int[] GetMultiplesOfSevenAscending()
+        {
+            return numbers.Where(x => x % 7 == 0).OrderBy(x => x).ToArray();
+        }
+        public int[] GetMultiplesOfEightDescending()
+        {
+            return numbers.Where(x => x % 8 == 0).OrderByDescending(x => x).ToArray();
+        }
+    }
+}
---
+++ DZ_15/Program.cs
@@ -0,0 +1,25 @@
+﻿using DZ_15;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+MyArray myArray = new MyArray(numbers);
+
+int[] allIntegers = myArray.GetAllIntegers();
+int[] evenNumbers = myArray.GetEvenNumbers();
+int[] oddNumbers = myArray.GetOddNumbers();
+int value = 5;
+int[] numbersGreaterThanValue = myArray.GetNumbersGreaterThan(value);
+int start = 3;
+int end = 8;
+int[] numbersInRange = myArray.GetNumbersInRange(start, end);
+int[] multiplesOfSeven = myArray.GetMultiplesOfSevenAscending();
+int[] multiplesOfEight = myArray.GetMultiplesOfEightDescending();
+
+Console.WriteLine("Усі цілі числа: " + string.Join(", ", allIntegers));
+Console.WriteLine("Парні числа: " + string.Join(", ", evenNumbers));
+Console.WriteLine("Непарні числа: " + string.Join(", ", oddNumbers));
+Console.WriteLine("Числа більше ніж " + value + ": " + string.Join(", ", numbersGreaterThanValue));
+Console.WriteLine("Числа в діапазоні " + start + " - " + end + ": " + string.Join(", ", numbersInRange));
+Console.WriteLine("Кратні семи (Висхідний): " + string.Join(", ", multiplesOfSeven));
+Console.WriteLine("Кратні восьми (Низхідний): " + string.Join(", ", multiplesOfEight));
